8f53146328a9859e6939eab9f6ae58a0098aa66e
sumanthp12 <sumanths387@gmail.com>
2026-05-31 17:46:47 +0530

    feat: add registration validation and duplicate email check

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,7 +2,26 @@ const User = require("../models/User");
 
 const registerUser = async (req, res) => {
   try {
+
     const { name, email, password, role } = req.body;
+
+    // Validation
+
+    if (!name || !email || !password || !role) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    // Duplicate Check
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
 
     const user = await User.create({
       name,
