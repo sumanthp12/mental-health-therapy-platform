293a28f92a2b1dfc3aa9f00828924564990f7d75
sumanthp12 <sumanths387@gmail.com>
2026-06-01 23:11:07 +0530

    feat: add jwt authentication middleware and protected profile route

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -109,7 +109,27 @@ const loginUser = async (req, res) => {
   }
 };
 
+const getProfile = async (req, res) => {
+
+  try {
+
+    const user = await User.findById(req.user.id)
+      .select("-password");
+
+    res.status(200).json(user);
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message,
+    });
+
+  }
+
+};
+
 module.exports = {
   registerUser,
   loginUser,
+  getProfile,
 };
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const protect = require("../middleware/authMiddleware");
+const { registerUser, loginUser, getProfile, } = require("../controllers/userController");
 
-const { registerUser, loginUser, } = require("../controllers/userController");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
+router.get("/profile", protect, getProfile);
 module.exports = router;
